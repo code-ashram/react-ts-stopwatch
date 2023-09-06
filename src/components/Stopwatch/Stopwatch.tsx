bfbd6edd7577747a67e82f402071addf3960900f
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-09-06 16:23:21 +0300

    add first  functionality

--- src/components/Stopwatch/Stopwatch.tsx
+++ src/components/Stopwatch/Stopwatch.tsx
@@ -1,23 +1,52 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 import cn from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStop, faPlay } from '@fortawesome/free-solid-svg-icons'
+import Time from '../../Models/Time.ts'
 
 import styles from './Stopwatch.module.css'
 
 const Stopwatch: FC = () => {
+  const [time, setTime] = useState<Time>({ seconds: 0, minutes: 0, hours: 0 })
+  const [isActive, setIsActive] = useState<boolean>(false)
+
+  const handleStart = () => {
+    setIsActive(true)
+  }
+
+  const handleStop = () => {
+    setIsActive(false)
+  }
+
+  useEffect(() => {
+    let intervalId: string | number | NodeJS.Timeout | undefined
+
+    if (isActive) {
+      intervalId = setInterval(
+        () => setTime(
+          (prevTime) => ({ ...prevTime, seconds: prevTime.seconds + 1 })
+        ), 1000)
+    } else {
+      clearInterval(intervalId)
+      setTime(
+        (prevTime) => ({ ...prevTime })
+      )
+    }
+
+    return () => clearInterval(intervalId)
+  }, [isActive])
 
   return <div className={styles.stopwatch}>
-    <div className={styles.stopwatchDisplay}>00:00:00</div>
+    <div className={styles.stopwatchDisplay}>{time.hours}:{time.minutes}:{time.seconds}</div>
 
     <div className={styles.controlPanel}>
-      <button className={cn(styles.btn)}>
-        <FontAwesomeIcon icon={faStop} size="xl" style={{color: "#2b4371",}} />
+      <button className={cn(styles.btn)} onClick={handleStop}>
+        <FontAwesomeIcon icon={faStop} size="xl" style={{ color: '#2b4371' }} />
       </button>
 
-      <button className={cn(styles.btn)}>
-        <FontAwesomeIcon icon={faPlay} size="xl" style={{color: "#2b4371",}}/>
+      <button className={cn(styles.btn)} onClick={handleStart}>
+        <FontAwesomeIcon icon={faPlay} size="xl" style={{ color: '#2b4371' }} />
       </button>
     </div>
   </div>
