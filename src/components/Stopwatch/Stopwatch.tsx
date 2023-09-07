06509668c164559c74d00389e3f79122b761742e
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-09-07 16:21:38 +0300

    add reset

--- src/components/Stopwatch/Stopwatch.tsx
+++ src/components/Stopwatch/Stopwatch.tsx
@@ -2,7 +2,7 @@ import { FC, useEffect, useState } from 'react'
 
 import cn from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStop, faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faPause, faPlay, faArrowRotateRight } from '@fortawesome/free-solid-svg-icons'
 import { hour, minute } from '../../Constants/time.ts'
 
 import styles from './Stopwatch.module.css'
@@ -11,12 +11,12 @@ const Stopwatch: FC = () => {
   const [seconds, setSeconds] = useState<number>(0)
   const [isActive, setIsActive] = useState<boolean>(false)
 
-  const handleStart = () => {
-    setIsActive(true)
+  const handleToggleStart = () => {
+    setIsActive(!isActive)
   }
 
-  const handleStop = () => {
-    setIsActive(false)
+  const handleReset = () => {
+    setSeconds(0)
   }
 
   useEffect(() => {
@@ -37,17 +37,17 @@ const Stopwatch: FC = () => {
   return <div className={styles.stopwatch}>
     <div className={styles.stopwatchDisplay}>
       {hours}
-      :{minutes.toString().padStart(2, "0")}
-      :{seconds.toString().padStart(2, "0")}
+      :{minutes.toString().padStart(2, '0')}
+      :{seconds.toString().padStart(2, '0')}
     </div>
 
     <div className={styles.controlPanel}>
-      <button className={cn(styles.btn)} onClick={handleStop}>
-        <FontAwesomeIcon icon={faStop} size="xl" style={{ color: '#2b4371' }} />
+      <button className={cn(styles.btn)} onClick={handleToggleStart}>
+        <FontAwesomeIcon icon={isActive ? faPause : faPlay} size="xl" style={{ color: '#2b4371' }} />
       </button>
 
-      <button className={cn(styles.btn)} onClick={handleStart}>
-        <FontAwesomeIcon icon={faPlay} size="xl" style={{ color: '#2b4371' }} />
+      <button className={cn(styles.btn)} onClick={handleReset}>
+        <FontAwesomeIcon icon={faArrowRotateRight} size="xl" style={{ color: '#2b4371' }} />
       </button>
     </div>
   </div>
